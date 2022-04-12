921d167f0a9b2f3b0bf7ae77d949c6f3d15d16b6
Fred19930510 <102570118+Fred19930510@users.noreply.github.com>
2022-04-12 18:59:35 +0800

    Add files via upload

---
+++ controller.js
@@ -0,0 +1,24 @@
+var now_theme = 0;
+
+function setGlobalVariable(variable_name, variable_value){
+	document.documentElement.style.setProperty(variable_name, variable_value);
+}
+
+function switch_theme(){
+	if(now_theme==0){
+		setGlobalVariable("--background_color", "#26333A");
+		setGlobalVariable("--text_color", "#FFFFFF");
+		setGlobalVariable("--link_color", "#FBB317");
+		now_theme=2;
+	}else if(now_theme==2){
+		setGlobalVariable("--background_color", "#DDEFE3");
+		setGlobalVariable("--text_color", "#2A484E");
+		setGlobalVariable("--link_color", "#CE4A50");
+		now_theme=1;
+	}else if(now_theme==1){
+		setGlobalVariable("--background_color", "#FFFFFF");
+		setGlobalVariable("--text_color", "#000000");
+		setGlobalVariable("--link_color", "#4E00FF");
+		now_theme=0;
+	}
+}
